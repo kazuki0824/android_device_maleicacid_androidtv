2d92f7a28ab861ab6d2b3d2557ddf98cd52112a0
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-03-08 21:43:34 +0900

    Update qemu_tv_virtio.mk

--- qemu_tv_virtio.mk
+++ qemu_tv_virtio.mk
@@ -1,6 +1,6 @@
 # requirement ID: qemu-tv-virtio / build product: qemu_tv_virtio
 
-$(call inherit-product, vendor/lineage/build/target/product/lineage_virtio_x86_64_tv.mk)
+$(call inherit-product, device/virt/virtio_x86_64_tv/lineage_virtio_x86_64_tv.mk)
 $(call inherit-product, device/maleicacid/androidtv/common.mk)
 
 PRODUCT_NAME := lineage_qemu_tv_virtio
