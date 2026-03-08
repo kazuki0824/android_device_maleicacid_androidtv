82f4f6cc592e51d01cee6bf0fe0f353b6f7f3350
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-03-08 11:43:06 +0900

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/r86s_tv_virtio.mk \
-    $(LOCAL_DIR)/qemu_tv_virtio.mk
+    lineage_r86s_tv_virtio:$(LOCAL_DIR)/r86s_tv_virtio.mk \
+    lineage_qemu_tv_virtio:$(LOCAL_DIR)/qemu_tv_virtio.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_r86s_tv_virtio-ap2a-userdebug \
