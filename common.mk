24ad6304b1768e150d55458af5b9d342ed00a94b
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-03-29 13:02:51 +0900

    Update common.mk

--- common.mk
+++ common.mk
@@ -2,10 +2,20 @@
 
 PRODUCT_CHARACTERISTICS := tv
 
-# GApps variant
-GMS_VARIANT := full
+
+ANDROID_USE_WIDEVINE := true
+ifeq ($(ANDROID_USE_WIDEVINE),true)
+$(call inherit-product-if-exists, vendor/google/proprietary/widevine-prebuilt/widevine.mk)
+endif
+
+ANDROID_USE_INTEL_HOUDINI := true
+ifeq ($(ANDROID_USE_INTEL_HOUDINI),true)
+$(call inherit-product-if-exists, vendor/intel/proprietary/houdini/houdini.mk)
+$(call inherit-product-if-exists, vendor/intel/proprietary/houdini/native_bridge_arm_on_x86.mk)
+endif
 
 # WayDroid-ATV GApps for Android TV x86_64
+GMS_VARIANT := full
 $(call inherit-product, vendor/gapps_tv/x86_64/x86_64-vendor.mk)
 
 PRODUCT_BRAND := maleicacid
